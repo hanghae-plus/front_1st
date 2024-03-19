d140a998744d1f6382fbf65a78364e87066bc4d8
gay0ung <58800923+gay0ung@users.noreply.github.com>
2024-03-20 00:01:18 +0900

    w2-과제A

--- packages/chapter2/src/render.js
+++ packages/chapter2/src/render.js
@@ -1,9 +1,20 @@
 export function jsx(type, props, ...children) {
-  return {}
+  // 가상 DOM을 생성
+  return {type, props, children}
 }
 
 export function createElement(node) {
   // jsx를 dom으로 변환
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+
+  const element = document.createElement(node.type);
+  updateAttributes(element, node.props, {});
+  node.children.forEach(child => {
+    element.appendChild(createElement(child));
+  });
+  return element;
 }
 
 function updateAttributes(target, newProps, oldProps) {
@@ -12,33 +23,66 @@ function updateAttributes(target, newProps, oldProps) {
   //     다음 속성으로 넘어감 (변경 불필요)
   //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
   //     target에 해당 속성을 새 값으로 설정
+  Object.keys(newProps).forEach(key => {
+    if (newProps[key] !== oldProps[key]) {
+      target.setAttribute(key, newProps[key]);
+    }
+  });
 
   // oldProps을 반복하여 각 속성 확인
   //   만약 newProps들에 해당 속성이 존재한다면
   //     다음 속성으로 넘어감 (속성 유지 필요)
   //   만약 newProps들에 해당 속성이 존재하지 않는다면
   //     target에서 해당 속성을 제거
+  Object.keys(oldProps).forEach(key => {
+    if (!newProps[key]) {
+      target.removeAttribute(key);
+    }
+  });
 }
 
 export function render(parent, newNode, oldNode, index = 0) {
+  // JSX로 생성한 가상 dom을 실제 dom 으로 변환하고 렌더링 하는 역할
+
   // 1. 만약 newNode가 없고 oldNode만 있다면
   //   parent에서 oldNode를 제거
   //   종료
+  if (!newNode && oldNode) {
+    parent.removeChild(parent.childNodes[index]);
+    return;
+  }
 
   // 2. 만약 newNode가 있고 oldNode가 없다면
   //   newNode를 생성하여 parent에 추가
   //   종료
+  if (newNode && !oldNode) {
+    parent.appendChild(createElement(newNode));
+    return;
+  }
 
   // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
   //   oldNode를 newNode로 교체
   //   종료
+  if ((typeof newNode === string && typeof oldNode === string) && newNode !== oldNode) {
+    parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+    return;
+  }
 
   // 4. 만약 newNode와 oldNode의 타입이 다르다면
   //   oldNode를 newNode로 교체
   //   종료
+  if (newNode.type !== oldNode.type) {
+    parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+    return;
+  }
 
   // 5. newNode와 oldNode에 대해 updateAttributes 실행
+  updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
 
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
   //   각 자식노드에 대해 재귀적으로 render 함수 호출
+  const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+  for (let i = 0; i < maxLength; i++) {
+    render(parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+  }
 }
