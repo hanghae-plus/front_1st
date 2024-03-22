b7363a8e1086bf41d6d51d804245025a67827dd9
glassk <glassk0724@gmail.com>
2024-03-22 23:59:33 +0900

    feat: 과제 C(나만의 React 만들기) 구현

--- packages/chapter2/src/MyReact.js
+++ packages/chapter2/src/MyReact.js
@@ -1,11 +1,32 @@
-import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { createHooks } from './hooks';
+import { render as updateElement } from './render';
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  const render = ($root, rootComponent) => {
+    while ($root.firstChild) {
+      $root.removeChild($root.firstChild);
+    }
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+    const component = () => updateElement($root, rootComponent());
+    resetHookContext();
+    component();
+    _render.current = component;
+  };
+
+  const _render = {
+    current: null,
+  };
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(() => {
+    if (_render.current) {
+      resetHookContext();
+      _render.current();
+    }
+  });
 
   return { render, useState, useMemo };
 }
