e0fc1f509cb510b07495812a568513fad74439a6
김유현 <kinyuhyun@gim-yuhyeons-MacBook-Air.local>
2024-03-22 23:07:55 +0900

    2주차 과제 B useState

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,30 @@
 export function createHooks(callback) {
-  const useState = (initState) => {
-    return [];
+  let hooks = [];
+  let index = 0;
+
+  function useState(initialState) {
+    const state = hooks[index] || initialState;
+    const localIdx = index;
+
+    const setState = (val) => {
+      if (val !== hooks[localIdx]) {
+        hooks[localIdx] = val;
+        resetContext();
+      }
+    };
+
+    index++;
+    return [state, setState];
+  }
+
+  const resetContext = () => {
+    index = 0;
+    callback();
   };
 
   const useMemo = (fn, refs) => {
     return fn();
   };
-
-  const resetContext = () => {
-
-  }
 
   return { useState, useMemo, resetContext };
 }
