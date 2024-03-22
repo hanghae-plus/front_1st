48270987c64810bb9bbd8ac09522d9aff55e0fc5
glassk <glassk0724@gmail.com>
2024-03-22 23:09:36 +0900

    feat: 과제 B(useState, useMemo 만들기) 구현

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,61 @@
 export function createHooks(callback) {
+  const states = [];
+  let statesIndex = 0;
+
+  const memos = [];
+  let memosIndex = 0;
+
   const useState = (initState) => {
-    return [];
+    const currentIndex = statesIndex;
+
+    if (states[currentIndex] === undefined) {
+      states[currentIndex] = initState;
+    }
+
+    const setState = (newState) => {
+      if (states[currentIndex] === newState) return;
+
+      states[currentIndex] = newState;
+      resetContext();
+
+      return callback();
+    };
+
+    statesIndex += 1;
+
+    return [states[currentIndex], setState];
   };
 
-  const useMemo = (fn, refs) => {
-    return fn();
+  const useMemo = (fn, deps) => {
+    const currentIndex = memosIndex;
+    const hasChanged =
+      memos[currentIndex] === undefined ||
+      !isEqualTwoArrays(memos[currentIndex].deps, deps);
+
+    if (hasChanged) {
+      const newValue = fn();
+      memos[currentIndex] = { deps, value: newValue };
+      memosIndex += 1;
+
+      return newValue;
+    } else {
+      memosIndex += 1;
+
+      return memos[currentIndex].value;
+    }
   };
 
   const resetContext = () => {
+    statesIndex = 0;
+    memosIndex = 0;
+  };
 
-  }
+  const isEqualTwoArrays = (arr1, arr2) => {
+    return (
+      arr1.length === arr2.length &&
+      arr1.every((value, index) => value === arr2[index])
+    );
+  };
 
   return { useState, useMemo, resetContext };
 }
