e6375f6b93114e300a28e9771927e8f98878b823
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-03-23 17:49:51 +0900

    Solve 2e

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,26 @@
-export const 구독 = fn => {
-  fn();
-}
+// const _callbacks = [];
+const _propsObservers = {};
+let _currentCallback;
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 구독 = (fn) => {
+  _currentCallback = fn;
+  fn();
+  _currentCallback = null;
+};
+
+export const 발행기관 = (obj) => {
+  return new Proxy(obj, {
+    get(target, prop) {
+      if (_currentCallback) {
+        _propsObservers[prop] ??= [];
+        _propsObservers[prop].push(_currentCallback);
+      }
+      return target[prop];
+    },
+    set(target, prop, value) {
+      target[prop] = value;
+      _propsObservers[prop].forEach((fn) => fn());
+      return true;
+    },
+  });
+};
