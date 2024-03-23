4a20f41a5921ca9f1afea3eac800ce9d87b34469
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-03-23 22:12:56 +0900

    chapter2 E ver1.0

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,34 @@
+let listener = {};
+let currentCallback;
+let store = new WeakMap();
+
 export const 구독 = (fn) => {
-  fn();
+  currentCallback = fn;
+  return fn();
 };
 
 export const 발행기관 = (obj) => {
-  return obj;
+  const newObject = new Proxy(obj, {
+    set: function (target, p, value, receiver) {
+      target[p] = value;
+      listener[p]?.forEach((cb) => {
+        cb();
+      });
+      return true;
+    },
+
+    get: function (target, p, receiver) {
+      if (typeof currentCallback === "function") {
+        listener[p] =
+          listener[p] === undefined
+            ? [currentCallback]
+            : new Set([...listener[p], currentCallback]);
+      }
+      return target[p];
+    },
+  });
+
+  store.set(obj, newObject);
+
+  return newObject;
 };
