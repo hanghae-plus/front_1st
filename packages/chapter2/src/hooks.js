f9ea5967c134a4da10fb2ffd49c132e8ed1bda01
suminllll <krlatnalsl01@gmail.com>
2024-03-24 15:07:30 +0900

    과제 B useState, useMemo

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,60 @@
 export function createHooks(callback) {
+  const stateContext = {
+    current: 0,
+    states: [],
+  };
+
+  const memoContext = {
+    current: 0,
+    memos: [],
+  };
+
   const useState = (initState) => {
-    return [];
+    const { current, states } = stateContext;
+
+    states[current] = states[current] ?? initState;
+
+    const setState = (newValue) => {
+      if (newValue === states[current]) return;
+      states[current] = newValue;
+      callback();
+    };
+
+    stateContext.current += 1;
+    return [states[current], setState];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    const { current, memos } = memoContext;
+    memoContext.current += 1;
+
+    const memo = memos[current];
+
+    const resetAndReturn = () => {
+      const value = fn();
+
+      memos[current] = {
+        value,
+        refs,
+      };
+      return value;
+    };
+
+    if (!memo) {
+      return resetAndReturn();
+    }
+
+    if (refs.length > 0 && memo.refs.find((v, k) => v !== refs[k])) {
+      return resetAndReturn();
+    }
+
+    return memo.value;
   };
 
   const resetContext = () => {
-
-  }
+    stateContext.current = 0;
+    memoContext.current = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
