388d4a16e3d7b6f2b5622f49290a40816e8ef0d9
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-03-24 14:51:32 +0900

    More readability

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -6,12 +6,14 @@ export const 발행기관 = (obj) => {
   return new Proxy(obj, {
     get(target, prop) {
       _propsObservers[prop] ??= [];
+
       if (
         _currentCallback &&
         !_propsObservers[prop].includes(_currentCallback)
       ) {
         _propsObservers[prop].push(_currentCallback);
       }
+
       return target[prop];
     },
     set(target, prop, value) {
