5d5fdb41e6f6914697b03e701934a1dbcd3fe1d7
choi1five <cwo1401@gmail.com>
2024-03-26 23:20:28 +0900

    3주차 과제E 구현

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,26 @@
-export const 구독 = fn => {
-  fn();
-}
+let subscribeFn;
+const subscriber = {};
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 구독 = (fn) => {
+  subscribeFn = fn;
+  fn();
+};
+
+export const 발행기관 = (obj) => {
+  const proxyObj = new Proxy(obj, {
+    get(target, prop) {
+      if (!(prop in target)) throw new Error("존재하지 않는 프로퍼티 입니다.");
+
+      if (subscribeFn) subscriber[prop] = new Set([...(subscriber[prop] ?? []), subscribeFn]);
+      return target[prop];
+    },
+    set(target, prop, value) {
+      if (!(prop in target)) return false;
+
+      target[prop] = value;
+      subscriber[prop].forEach((fn) => fn());
+      return true;
+    },
+  });
+  return proxyObj;
+};
