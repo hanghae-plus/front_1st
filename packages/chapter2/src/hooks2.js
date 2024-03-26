1e06e922926e47f93fa7662170bfcb83a14a012e
qoqomi <energyy044@gmail.com>
2024-03-26 21:58:57 +0900

    init node module gitignore에 추가

--- packages/chapter2/src/hooks2.js
+++ packages/chapter2/src/hooks2.js
@@ -1,4 +1,3 @@
-
 export function createHooks(callback) {
   const stateContext = {
     current: 0,
@@ -24,7 +23,7 @@ export function createHooks(callback) {
     const setState = (newState) => {
       if (newState === states[current]) return;
       states[current] = newState;
-      callback();
+      requestAnimationFrame(callback());
     };
 
     return [states[current], setState];
