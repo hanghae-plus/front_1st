7a14a142613655151afa35203c83496b46f1f498
mincheol <mincheol@douzone.com>
2024-03-26 21:58:21 +0900

    feat: 과제 E 완료

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,26 @@
+const observers = {};
+let currentCallback = null;
 export const 구독 = fn => {
+  currentCallback = fn;
   fn();
+  currentCallback = null;
 }
 
 export const 발행기관 = obj => {
-  return obj;
+  const state = obj;
+  const handler = {
+    get(target, prop, receiver) {
+      if (currentCallback !== null) {
+        if(Array.isArray(observers[prop])) observers[prop].push(currentCallback);
+        else observers[prop] = [currentCallback];
+      }
+      return Reflect.get(target, prop, receiver);
+    },
+    set(target, prop, val, receiver) {
+      const res = Reflect.set(target, prop, val, receiver);
+      if (Array.isArray(observers[prop])) observers[prop].forEach((fn) => fn());
+      return res;
+    }
+  }
+  return new Proxy(state, handler);
 }
