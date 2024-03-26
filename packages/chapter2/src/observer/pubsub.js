d78740ca11502520dd8cb859bbe6d7ccd04ca606
rose <rosefor2ne@gmail.com>
2024-03-26 17:48:07 +0900

    ch2 과제 E (Object.defineProperty)

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,28 @@
+let currentCallback = null;
+
 export const 구독 = fn => {
+  currentCallback = fn;
   fn();
+  currentCallback = null;
 }
 
 export const 발행기관 = obj => {
+  Object.keys(obj).forEach(key => {
+    let _value = obj[key];
+    const observers = new Set();
+
+    Object.defineProperty(obj, key, {
+      get() {
+        if(currentCallback) observers.add(currentCallback);
+        return _value;
+      },
+      set(value) {
+        if(_value === value) return;
+        _value = value;
+        observers.forEach(fn => fn());
+      }
+    })
+  })
+
   return obj;
 }
