02ffe89a3ec137c36d06da5cf36ae1ab265be836
hyeonjeong <hjung9248@gmail.com>
2024-03-27 21:48:50 +0900

    state managing with pub-sub pattern

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,32 @@
-export const 구독 = fn => {
+export const 구독 = (fn) => {
+  subscriber = fn;
   fn();
-}
+};
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 발행기관 = (obj) => {
+  const observers = new Map();
+
+  const state = new Proxy(obj, {
+    set(target, key, value) {
+      target[key] = value;
+      notifyStateChanged(key);
+      return true;
+    },
+    get(target, key) {
+      if (!observers.has(key)) {
+        observers.set(key, new Set());
+      }
+      if (subscriber !== null && !observers.get(key).has(subscriber)) {
+        observers.get(key).add(subscriber);
+      }
+      return target[key];
+    },
+  });
+
+  const notifyStateChanged = (key) => {
+    observers.get(key).forEach((fn) => fn());
+  };
+  return state;
+};
+
+let subscriber = null;
