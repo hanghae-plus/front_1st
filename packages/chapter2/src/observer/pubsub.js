2a61713aaea5e8d1e69ddab384ad144d17359ece
ChyunKim <co1995@naver.com>
2024-03-27 22:17:48 +0900

    add: 과제 E 구현

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,38 @@
-export const 구독 = fn => {
-  fn();
-}
+const pubSubBroker = {
+  currentSub: null,
+  subscribers: {},
+  addSubscriber: (key) => {
+    if (!pubSubBroker.subscribers[key]) pubSubBroker.subscribers[key] = new Set();
+    pubSubBroker.subscribers[key].add(pubSubBroker.currentSub);
+  },
+  notifySubscribers: (key) => {
+    pubSubBroker.subscribers[key].forEach((fn) => fn());
+  },
+};
 
-export const 발행기관 = obj => {
+export const 구독 = (fn) => {
+  pubSubBroker.currentSub = fn;
+  fn();
+  pubSubBroker.currentSub = null;
+};
+
+export const 발행기관 = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    let _value = obj[key];
+    const subKey = key;
+
+    Object.defineProperty(obj, key, {
+      get() {
+        if (pubSubBroker.currentSub) {
+          pubSubBroker.addSubscriber(subKey);
+        }
+        return _value;
+      },
+      set(value) {
+        _value = value;
+        pubSubBroker.notifySubscribers(subKey);
+      },
+    });
+  });
   return obj;
-}
+};
