47a499af65b0c882a628abe69f2bb20fe0abdc39
Min Seung Jung <alstmd94@hanyang.ac.kr>
2024-03-27 22:01:32 +0900

    feat: solve test E

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,32 @@
-export const 구독 = fn => {
+export const 구독 = (fn) => {
+  handler.message = fn;
   fn();
-}
+};
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 발행기관 = (obj) => {
+  return new Proxy(obj, handler);
+};
+
+const handler = {
+  message: undefined,
+  subscription: {},
+
+  get(target, prop, receiver) {
+    if (!this.subscription[prop]) {
+      this.subscription[prop] = [];
+    }
+    const cb = this.message;
+    if (!this.subscription[prop].includes(cb)) {
+      this.subscription[prop].push(cb);
+    }
+
+    return Reflect.get(...arguments);
+  },
+  set(target, prop, value, receiver) {
+    Reflect.set(...arguments);
+
+    this.subscription[prop].forEach((fn) => fn());
+
+    return receiver;
+  },
+};
