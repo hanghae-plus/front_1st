5c5f165a8be7e7507abcf42bd35130f8c73c6933
sooyoung <sooyoung159@naver.com>
2024-03-28 19:33:53 +0900

    과제 E, F

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,25 @@
+let subscriber = null
+let 구독자들 = new Set()
 export const 구독 = fn => {
+  subscriber = fn
   fn();
 }
 
+
 export const 발행기관 = obj => {
-  return obj;
+  const state = {}
+  Object.keys(obj).forEach(key =>   Object.defineProperty(state, key, {
+    get:() =>{
+      구독자들[key] = 구독자들[key] || new Set()
+      구독자들[key].add(subscriber)
+
+      return obj[key]
+    },
+    set: (val) => {
+      obj[key] = val
+      구독자들[key].forEach(x => x())
+    }
+  }))
+
+  return state;
 }
