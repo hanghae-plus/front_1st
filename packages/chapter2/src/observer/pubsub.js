94692bc26a3b28f41887c112e6161b549bc29cd5
lipton-web <cpt.lipton@gmail.com>
2024-03-28 23:50:01 +0900

    STEP5 과제E_최진식

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,29 @@
-export const 구독 = fn => {
-  fn();
-}
+let currentCallback = null;
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 구독 = (fn) => {
+  currentCallback = fn;
+  fn();
+};
+
+export const 발행기관 = (obj) => {
+  const state = {};
+  Object.keys(obj).forEach((key) => {
+    let currentValue = obj[key];
+    const observers = new Set();
+    Object.defineProperty(state, key, {
+      get() {
+        if (currentCallback) {
+          observers.add(currentCallback);
+        }
+        return currentValue;
+      },
+      set(value) {
+        if (currentValue !== value) {
+          currentValue = value;
+          observers.forEach((fn) => fn());
+        }
+      },
+    });
+  });
+  return state;
+};
