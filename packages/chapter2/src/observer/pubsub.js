845fa21735571238af0009b021069bdf409420a7
glassk <glassk0724@gmail.com>
2024-03-28 23:12:20 +0900

    feat: 과제 E(구독/발행 패턴 구현) 해결

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,31 @@
-export const 구독 = fn => {
-  fn();
-}
+let currentCallback = null;
 
-export const 발행기관 = obj => {
+export const 구독 = (fn) => {
+  currentCallback = fn;
+
+  fn();
+
+  currentCallback = null;
+};
+
+export const 발행기관 = (obj) => {
+  Object.entries(obj).forEach(([key, value]) => {
+    const observers = new Set();
+    Object.defineProperty(obj, key, {
+      get() {
+        if (currentCallback) {
+          observers.add(currentCallback);
+        }
+
+        return value;
+      },
+
+      set(newValue) {
+        value = newValue;
+        observers.forEach((fn) => fn());
+      },
+    });
+  });
+
   return obj;
-}
+};
